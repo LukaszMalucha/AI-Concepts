0793a009a892e298f422f71374e10e4ed59b3b83
Lukasz_Malucha <lucasmalucha@gmail.com>
2018-12-21 20:35:21 +0000

    brain

---
+++ minimizing_cost/brain.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+############################################################### Import Libraries
+
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.optimizers import Adam
+
+
+################################################################# Building Brain
+
+
+
+
