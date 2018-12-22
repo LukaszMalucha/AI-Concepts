bec7d8491c6856df2a00ba7f9f928cadb895f7cf
Lukasz_Malucha <lucasmalucha@gmail.com>
2018-12-22 11:06:02 +0000

    brain

--- minimizing_cost/brain.py
+++ minimizing_cost/brain.py
@@ -11,5 +11,19 @@ from keras.optimizers import Adam
 ################################################################# Building Brain
 
 
-
+class Brain(object):
+    
+    def __init__(self, learning_rate = 0.001, number_actions = 5):
+        
+        self.learning_rate = learning_rate        
+        states = Input(shape = (3,))                                            ## three rows, one column
+        x = Dense(units = 64, activation = 'sigmoid')(states)                   ## add states to create full connection with previous layer
+        y = Dense(units = 32, activation = 'sigmoid')(x)
+        q_values = Dense(units = number_actions, activation = 'softmax')(y)     ## output layer        
+        self.model = Model(inputs = states, outputs = q_values)
+        self.model.compile(loss = 'mse', optimizer = Adam(lr = learning_rate))
+        
+        
+        
+        
 
