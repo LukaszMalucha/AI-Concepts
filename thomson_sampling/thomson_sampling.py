8dd3998293e19934d393f00346ccee063cc3d535
Lukasz_Malucha <lucasmalucha@gmail.com>
2018-12-25 18:54:37 +0000

    thomson

--- thomson_sampling/thomson_sampling.py
+++ thomson_sampling/thomson_sampling.py
@@ -37,56 +37,52 @@ total_reward_ts = 0
 numbers_of_rewards_1 = [0] * d
 numbers_of_rewards_0 = [0] * d
 
-for n in range(0, N):
+for n in range(0, N):           ## for each round
     # Random Strategy
     strategy_rs = random.randrange(d)            ## select random 0-8 strategy
-    strategies_selected_rs.append(strategy_rs)   
-    reward_rs = X[n, strategy_rs]
-    total_reward_rs += reward_rs
+    strategies_selected_rs.append(strategy_rs)   ## append to list of random strategies
+    reward_rs = X[n, strategy_rs]                ## compare selected action with "real life simulation" X and get assigned reward
+    total_reward_rs += reward_rs                 ## get total reward
+    
+    # Thomson Sampling
+    strategy_ts = 0
+    max_random = 0
+    for i in range(0, d):                        ## for each strategy
+        ## compare how many times till now that strategy recieved 1 or 0 to get the Random Draw
+        random_beta = random.betavariate(numbers_of_rewards_1[i] +1, numbers_of_rewards_0[i] +1)
+        # update random beta for each strategy
+        if random_beta > max_random:       
+            max_random = random_beta
+            strategy_ts = i 
+            
+    reward_ts = X[n, strategy_ts]    ## compare selected action with "real life simulation" X and get assigned reward 
+    # update number of rewards
+    if reward_ts == 1:
+        numbers_of_rewards_1[strategy_ts] += 1
+    else:
+        numbers_of_rewards_0[strategy_ts] += 1
+    ## append to list of ts strategies    
+    strategies_selected_ts.append(strategy_ts)
+    ## accumulate total ts rewards
+    total_reward_ts += reward_ts
+    
+        
+####################################### Compute the Absolute and Relative Return    
+    
+absolute_return = (total_reward_ts - total_reward_rs)*100    ## each customer converion = 100 USD
+relative_return = (total_reward_ts - total_reward_rs) / total_reward_rs * 100
+    
+print("Absolute Return: {:.0f} $".format(absolute_return))    
+print("Relative Return: {:.0f} %".format(relative_return))       
     
     
     
+########################################### Plotting the Histogram of Selections
+
+plt.hist(strategies_selected_ts) 
+plt.title("Histogram of Selections")
+plt.xlabel('Strategy')
+plt.ylabel('Number of times the strategy was aselected')
+plt.show()
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
